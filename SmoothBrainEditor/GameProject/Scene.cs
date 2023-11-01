251390f2823e209a33eee51d5f6fa48bec1983ee
Brian Washington Jr <bw49078@gmail.com>
2023-11-01 11:41:50 -0500

    Created the skeleton of the scene class. Will add onto it when game entities are added.

---
+++ SmoothBrainEditor/GameProject/Scene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmoothBrainEditor.GameProject
+{
+    public class Scene : ViewModelBase
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set 
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public Project Project { get; private set; }
+
+    }
+}
